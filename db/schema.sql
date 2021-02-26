d8902f5d580eb4d21c0c6598387f682cc919650c
DaFonz <brandonjfontes@gmail.com>
2021-02-26 16:32:34 -0700

    set up mysql database and server

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("1", "Big Belly Burger", "1"),
+    ("2", "Barista Burger", "0"),
+    ("3", "Whats Gooda? Burger", "1");
