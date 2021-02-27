e18e231a42a14f2011a6f7e0199a5682e8c2850b
DaFonz <brandonjfontes@gmail.com>
2021-02-26 20:11:49 -0700

    set up handlebars and server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+const expbs = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
+
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
+
+
+
+app.listen(8080, () => {
+    console.log('Server is up at PORT ' + PORT);
+});
+
