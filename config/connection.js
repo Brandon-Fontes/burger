6517577985a958a7db19d11216c050ae51fe1bda
fattchris <cfontes@gmail.com>
2021-03-17 12:55:16 -0600

    changed mysql connection to pool

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+	connectionLimit: 5,
     host: "us-cdbr-east-03.cleardb.com",
     port: 3306,
     user: "bfb68d7fb9eca7",
@@ -8,7 +9,7 @@ var connection = mysql.createConnection({
     database: "heroku_2e9c7320e7b6078"
 });
 
-connection.connect(function(err) {
+connection.getConnection(function(err) {
     if (err) throw err;
     console.log("Connected!");
   });
